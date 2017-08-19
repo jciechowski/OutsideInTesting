4e2d2d838c8ec603934ee0ea6122161cc5306170
Jakub Ciechowski <sianex@gmail.com>
2017-08-19 09:01:26 +0200

    Add project files.

---
+++ OutsideInTesting/HomeJsonTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace OutsideInTesting
+{
+    public class HomeJsonTests
+    {
+        [Test]
+        public void GetResponseReturnCorrectStatusCode()
+        {
+            using (var client = HttpClientFactory.Create())
+            {
+                var response = client.GetAsync("").Result;
+
+                Assert.True(response.IsSuccessStatusCode, "Actual status code: " + response.StatusCode);
+            }
+        }
+
+
+    }
+}
---
+++ RunningJournalApi/JournalController.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace RunningJournalApi
+{
+    public class JournalController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse();
+        }
+    }
+}
